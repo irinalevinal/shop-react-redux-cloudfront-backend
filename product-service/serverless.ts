8f72d97ec2a672110c16b94e9c2084abe94f381b
Irina Levina <Irina_Levina@epam.com>
2023-04-12 22:54:30 +0300

    add parameters for dynamodb to serverless config

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -2,6 +2,7 @@ import type { AWS } from '@serverless/typescript';
 
 import getProductsList from '@functions/getProductsList';
 import getProductsById from '@functions/getProductsById';
+import createProduct from '@functions/createProduct';
 
 const serverlessConfiguration: AWS = {
   service: 'product-service',
@@ -17,14 +18,35 @@ const serverlessConfiguration: AWS = {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
-    
+    iam: {
+      role: {
+        statements: [{
+          Effect: "Allow",
+          Action: [
+            "dynamodb:DescribeTable",
+            "dynamodb:Query",
+            "dynamodb:Scan",
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem",
+          ],
+          Resource: 'arn:aws:dynamodb:eu-central-1:*:*'
+          // 'arn:aws:dynamodb:eu-central-1*:*'
+          // 'arn:aws:dynamodb:us-east-1:*:*'
+        }]
+      }
+    },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      PRODUCTS_TABLE_NAME: 'products',
+      STOCKS_TABLE_NAME: 'stocks'
     },
   },
+  
   // import the function via paths
-  functions: { getProductsList, getProductsById },
+  functions: { getProductsList, getProductsById, createProduct},
   package: { individually: true },
   custom: {
     esbuild: {
