352b91b4ad6d732bba8fc72185d7804f919b8dd0
Irina Levina <Irina_Levina@epam.com>
2023-04-25 23:25:33 +0300

    Create SNS

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -4,6 +4,8 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import schema from './schema';
 import { internalServerErrorResponse } from '../../libs/api-gateway';
 import s3Repository from '@libs/s3.repository';
+import { SQS } from 'aws-sdk';
+
 
 const importFileParser: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   try {
@@ -11,11 +13,25 @@ const importFileParser: ValidatedEventAPIGatewayProxyEvent<typeof schema> = asyn
     const bucketName = `${process.env.S3_BUCKET_PRODUCTS}`;
     const key = (event as any).Records[0].s3.object.key;
 
-    console.log('===== KEY =====', key);
+    const sqs = new SQS({ region: `${process.env.SQS_REGION}` });
 
-    const file = await s3Repository.getStreamObject(key, bucketName);
+    const stream = await s3Repository.getStreamObject(key, bucketName);
 
-    console.log('--- file chunks ---', file)
+    for await (const data of stream) {
+      await sqs
+        .sendMessage({
+          QueueUrl: `${process.env.SQS_PRODUCT_QUEUE}`,
+          MessageBody: JSON.stringify(data),
+        },
+        (error, data) => {
+                  if (error) {
+                    console.log('ERROR IN SQS MESSAGE POST', error)
+                  } else {
+                    console.log('DATA SENT INTO SQS', data)
+                  }
+                }
+        ).promise();;
+    }
 
     await s3Repository.copyObject(key, bucketName, 'uploaded/', 'parsed/');
     await s3Repository.deleteObject(key, bucketName);
